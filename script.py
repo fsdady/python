81a23893d74c3885b9379d259a282a347a1baf0b
fsdady <134836180+fsdady@users.noreply.github.com>
2023-09-09 09:31:43 +0330

    script.py

--- script.py
+++ script.py
@@ -3,6 +3,5 @@ import requests
 import time
 
 while True:
-    response = requests.get('https://api.telegram.org/bot6043246928:AAFAjhgyo8djiaR-erh2RNUnUM7mR7BkP4s/sendMessage?chat_id=5411173999&text=Hiii')
-    print(response.text)
+    print("ok")
     time.sleep(10)
