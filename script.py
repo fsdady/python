2627160ec8b2c02085aa22d3d0aee6b95e136f11
fsdady <134836180+fsdady@users.noreply.github.com>
2023-09-09 20:43:26 +0330

    script.py

--- script.py
+++ script.py
@@ -1,7 +1,22 @@
-#pylint:disable=E0001
-import requests
-import time
+# کد بررسی پروکسی
 
-while True:
-    print("ok")
-    time.sleep(10)
+import requests
+
+def check_proxy(proxy):
+    try:
+        response = requests.get("https://www.google.com", proxies={"https": proxy}, timeout=5)
+        if response.status_code == 200:
+            print(f"پروکسی {proxy} فعال است.")
+        else:
+            print(f"پروکسی {proxy} غیرفعال است.")
+    except:
+        print(f"پروکسی {proxy} غیرفعال است.")
+
+proxies = [
+    "164.92.96.237:3128",
+    "192.168.1.2:8080",
+    "192.168.1.3:8080"
+]
+
+for proxy in proxies:
+    check_proxy(proxy)
